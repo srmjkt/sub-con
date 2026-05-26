7b763ca1882a0a26bb37a5e45490247d408fa5bb
srmjkt <srm.hki.jakarta@gmail.com>
2026-05-26 11:48:59 +0700

    Add Next.js configuration

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    tsconfigPath: "./tsconfig.json",
+  },
+};
+
+export default nextConfig;
